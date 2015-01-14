5ef3d78ca17c42f489a5c6ca631da3b4165cc3be
MaxPlay <maxplayyt@gmail.com>
2015-01-14 01:36:57 +0100

    Alpha 0.0.2.1
    Added Weapon including Shootingeffect

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+    public ParticleSystem[] Effects;
+    public float AutoReloadTime;
+    private float TimePassed;
+
+    void Update()
+    {
+        if (Input.GetAxis("Fire1") != 0)
+            Shoot();
+    }
+
+    public void Shoot()
+    {
+        if (AutoReloadTime < TimePassed)
+        {
+            for(int i = 0; i < Effects.Length;i++)
+                Effects[i].Play();
+            TimePassed = 0;
+        }
+        else
+            TimePassed += Time.deltaTime;
+    }
+}
