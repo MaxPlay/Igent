e022e317bfd5b0c48ffc6c4596de8d55d382a3d3
Sheepy123 <kaufmann-peter@gmx.net>
2015-01-21 18:36:24 +0100

    FirstAI Approach
    Added simple AI with rigidbody

---
+++ Assets/Scripts/Characterscripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+
+	public int MaxHP = 100;
+	public float Regencooldown = 0f;
+	public float TimeUntilRegeneration = 5.0f;
+	public float CurrentRegencooldown = 0f;
+	public int HP = 100;
+
+
+	void Start () 
+	{
+	
+	}
+	public void TakeDamage(int _Damage)
+	{
+		HP -= _Damage;
+		Regencooldown = 0;
+	}
+	public void Regeneration()
+	{
+		if(HP < MaxHP)
+		HP += 1;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (TimeUntilRegeneration <= Regencooldown)
+			Regeneration ();
+		CurrentRegencooldown += Time.deltaTime;
+		if (Regencooldown > 10f)
+			Regencooldown = TimeUntilRegeneration+0.1f;
+	}
+}
