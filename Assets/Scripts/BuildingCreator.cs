e24899bfa6ab220ce3a8f23e3d290c986e16129c
MaxPlay <maxplayyt@gmail.com>
2015-01-22 17:35:05 +0100

    Alpha 0.0.3
    Added Procedural Buildings
    Added Walls (used for the buildings)
    Added Girders

---
+++ Assets/Scripts/BuildingCreator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class BuildingCreator : MonoBehaviour {
+
+    public GameObject Wall;
+    public GameObject WallWindow;
+    public GameObject WallDoor;
+    public GameObject Connect;
+    public GameObject CornerConvex;
+    public GameObject CornerConcave;
+    public GameObject RoofWall;
+    public GameObject RoofConnect;
+    public GameObject RoofCornerConvex;
+    public GameObject RoofCornerConcave;
+}
