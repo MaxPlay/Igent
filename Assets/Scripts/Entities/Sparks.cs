5d9dc783af6c0468c675d91c47089d858e3bf2e8
MaxPlay <maxplayyt@gmail.com>
2015-01-22 19:15:08 +0100

    Alpha 0.0.3.1
    Added Sparks
    Added Rawmodel of SRHTower

---
+++ Assets/Scripts/Entities/Sparks.cs
@@ -0,0 +1,13 @@
+﻿/** Code is poetry **/
+using UnityEngine;
+using System.Collections;
+
+public class Sparks : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        ParticleSystem particleSystem = gameObject.particleSystem;
+        if (particleSystem.time > 0.1f && particleSystem.particleCount == 0)
+            Destroy(gameObject);
+	}
+}
