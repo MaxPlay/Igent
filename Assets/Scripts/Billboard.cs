5cc1745e9536b9477e8b37af76dcadb837d705d9
MaxPlay <maxplayyt@gmail.com>
2015-01-28 04:34:17 +0100

    Alpha 0.0.3.3
    Added DistortionEffect
    Fires now have a heat distortion
    Distortionobject has its own billboarding script

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,16 @@
+﻿/** Code is poetry **/
+using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour {
+
+    void Update()
+    {
+        Vector3 CamRot = Camera.main.transform.rotation.eulerAngles;
+        Vector3 ThisRot = transform.rotation.eulerAngles;
+        Quaternion q = new Quaternion();
+        q.eulerAngles = new Vector3(-90, CamRot.y, 90);
+
+        transform.rotation = q;
+	}
+}
